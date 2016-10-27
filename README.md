e48431b5277f1eec42495ed8dfe3a9085cfae0da
Pierre Dargham <pierre.dargham@globalis-ms.com>
2016-10-27 18:51:57 +0200

    Add multisite / network-wide plugins support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Installation
 
-Parse below code to your wp-config/< WP_ENV >/options.php file.
+- Parse below code to your wp-config/< WP_ENV >/options.php file.
 
 ```PHP
 // WPG PLUGINS AUTOLADER : FORCE ENABLE
@@ -16,5 +16,18 @@ $wpg_force_plugins_disable = [
 	'bwp-minify/bwp-minify.php',
 ];
 define('WPG_FORCE_PLUGINS_DISABLE', serialize($wpg_force_plugins_disable));
+
+// WPG PLUGINS AUTOLADER : FORCE ENABLE NETWORK-WIDE
+$wpg_force_plugins_enable_multiste = [
+	'multisite-clone-duplicator/multisite-clone-duplicator.php',
+];
+define('WPG_FORCE_PLUGINS_NETWORK_ENABLE', serialize($wpg_force_plugins_enable_multiste));
+
+// WPG PLUGINS AUTOLADER : FORCE DISABLE NETWORK-WIDE
+$wpg_force_plugins_disable_multiste = [
+    'wordpress-mu-domain-mapping/domain_mapping.php',
+];
+define('WPG_FORCE_PLUGINS_NETWORK_DISABLE', serialize($wpg_force_plugins_disable_multiste));
 ```
-Modify $wpg_force_plugins_enable and $wpg_force_plugins_disable as you wish.
+
+- Modify $wpg_force_plugins_enable and $wpg_force_plugins_disable as you wish.
--- wpg-plugins-autoloader.php
+++ wpg-plugins-autoloader.php
@@ -9,8 +9,13 @@ Version:      1.0
 
 namespace WPG\PluginsAutoloader;
 
+// SINGLE SITE PLUGINS :
 add_filter('option_active_plugins', __NAMESPACE__ . '\\enable_plugins', 99);
 add_filter('option_active_plugins', __NAMESPACE__ . '\\disable_plugins', 99);
+
+// MULTISITE NETWORK-WIDE PLUGINS :
+add_filter('site_option_active_sitewide_plugins', __NAMESPACE__ . '\\enable_plugins_multisite', 99);
+add_filter('site_option_active_sitewide_plugins', __NAMESPACE__ . '\\disable_plugins_multisite', 99);
 
 function enable_plugins($plugins) {
 	if(!defined('WPG_FORCE_PLUGINS_ENABLE') || !is_serialized(WPG_FORCE_PLUGINS_ENABLE)) {
@@ -24,4 +29,23 @@ function disable_plugins($plugins) {
 		return $plugins;
 	}
 	return array_diff($plugins, unserialize(WPG_FORCE_PLUGINS_DISABLE));
+}
+
+function enable_plugins_multisite($plugins) {
+	if(!defined('WPG_FORCE_PLUGINS_NETWORK_ENABLE') || !is_serialized(WPG_FORCE_PLUGINS_NETWORK_ENABLE)) {
+		return $plugins;
+	}
+	foreach(unserialize(WPG_FORCE_PLUGINS_NETWORK_ENABLE) as $force_plugin) {
+		if(!isset($plugins[$force_plugin])) {
+			$plugins[$force_plugin] = time();
+		}
+	}
+	return $plugins;
+}
+
+function disable_plugins_multisite($plugins) {
+	if(!defined('WPG_FORCE_PLUGINS_NETWORK_DISABLE') || !is_serialized(WPG_FORCE_PLUGINS_NETWORK_DISABLE)) {
+		return $plugins;
+	}
+	return array_diff_key($plugins, array_flip(unserialize(WPG_FORCE_PLUGINS_NETWORK_DISABLE)));
 }
